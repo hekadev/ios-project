a07bf61b08c973a1bdc3b8f073aad0afb034b4e9
Hery Kang <heka.dev@gmail.com>
2016-07-12 19:09:08 +0700

    creating autolayout project

---
+++ autolayout/autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Hery Kang on 12/7/16.
+//  Copyright © 2016 hekadev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
